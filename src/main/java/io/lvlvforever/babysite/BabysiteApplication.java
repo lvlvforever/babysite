34043d240a12e106678d64faa21367298365004d
lvlv <lvlvbuaa@163.com>
2019-02-13 22:23:38 +0800

    initial

---
+++ src/main/java/io/lvlvforever/babysite/BabysiteApplication.java
@@ -0,0 +1,14 @@
+package io.lvlvforever.babysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabysiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabysiteApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/io/lvlvforever/babysite/BabysiteApplicationTests.java
@@ -0,0 +1,17 @@
+package io.lvlvforever.babysite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BabysiteApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
