ab3b8cdddaae4a3800824a989b6ba59ca3f56e36
lvlv <lvlvbuaa@163.com>
2019-02-14 23:18:19 +0800

    使用jquery bootstrap thymleaf

---
+++ src/main/java/io/lvlvforever/babysite/config/MongoConfig.java
@@ -0,0 +1,34 @@
+package io.lvlvforever.babysite.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+/**
+ * Created by lvlvforever on 2019/2/14.
+ */
+
+@Configuration
+public class MongoConfig {
+
+    @Autowired
+    MongoDbFactory mongoDbFactory;
+    @Autowired
+    MongoMappingContext mongoMappingContext;
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter() {
+        //remove _class
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return converter;
+    }
+}
